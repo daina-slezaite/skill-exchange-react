5de8c7786021a11cd91d80e3f75a7d494f230eaa
Daina Slezaite <daina.slezaite@gmail.com>
2021-02-21 20:34:26 +0100

    Add functionality to search skills

--- src/components/skills/AllSkills.js
+++ src/components/skills/AllSkills.js
@@ -9,7 +9,9 @@ export default class AllSkills extends Component {
         super(props);
         this.state = {
             allSkills: [],
-            favSkills: this.props.userInSession ? this.props.userInSession.favoriteSkills : []
+            favSkills: this.props.userInSession ? this.props.userInSession.favoriteSkills : [],
+            title: '',
+            category: ''
         }
     }
 
@@ -36,9 +38,52 @@ export default class AllSkills extends Component {
             .catch(error => console.log(error));
     }
 
+    handleInputChange = e => {
+        // const {name, value} = e.target;
+        // this.setState({[name]: value})
+        this.setState({title: e.target.value})
+        this.searchBar(e);
+    }
+
+    searchBar = (e) => {
+        axios.get(`http://localhost:5000/api/skills/search?title=${e.target.value}`)
+            .then(response => {
+                this.setState({allSkills: response.data})
+            })
+    }
+
+
+    // handleSelectChange = e => {
+    //     console.log(e.target.value)
+    //     this.setState({category: e.target.value})
+    //     axios.get(`http://localhost:5000/api/skills/search?category=${e.target.value}`)
+    //         .then(response => {
+    //             console.log(response.data);
+    //             this.setState({allSkills: response.data})
+    //         })
+    // }
+
     render() {
         return (
             <div>
+                <label>Search skills:</label>
+                <input type='text' name='title' value={this.state.title} onChange={this.handleInputChange} />
+                {/* <form>
+
+                </form>
+                <label>Filter by category:</label>
+                <select name='category' value={this.state.category} onChange={this.handleInputChange}>
+                    <option value="" defaultValue>Select your option</option>
+                    <option value='Graphic Design'>Graphic Design</option>
+                    <option value='Languages'>Languages</option>
+                    <option value='Music'>Music</option>
+                    <option value='Illustration'>Illustration</option>
+                    <option value='Lifestyle'>Lifestyle</option>
+                    <option value='Photography & Video'>Photography & Video</option>
+                    <option value='Business'>Business</option>
+                    <option value='Writing'>Writing</option>
+                    <option value='Fine Art'>Fine Art</option>
+                </select> */}
                 <ul>
                 {this.state.allSkills.map(skill => {
                     return(
