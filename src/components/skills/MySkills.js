274b336a5823675df88db01f6a98affe482d556c
Daina Slezaite <daina.slezaite@gmail.com>
2021-03-09 21:34:25 +0100

    Style myskills component

--- src/components/skills/MySkills.js
+++ src/components/skills/MySkills.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 import AddSkill from './AddSkill';
 import Popup from 'reactjs-popup';
 import 'reactjs-popup/dist/index.css';
+import './MySkills.scss';
 
 export default class MySkills extends Component {
 
@@ -40,21 +41,28 @@ export default class MySkills extends Component {
 
     render() {
         return (
-            <div>
+            <main className='my-skills'>
+                <div>
+                    <Link to='/skills'>Browse skills</Link>
+                    <Popup trigger={<button> Add new skill </button>} modal>
+                        <AddSkill getAllMySkills={() => this.getMySkills()} />
+                    </Popup>
+                </div>
                 <ul>
                 {this.state.mySkills.map(skill => {
                     return(
-                        <React.Fragment key={skill._id}>
-                        <li><Link to={`/skills/${skill._id}`}>{skill.title}</Link></li>
-                        <button onClick={(skillId)=> this.handleDelete(skill._id)}>Delete</button>
-                        </React.Fragment>
+                        <section className='my-skill-card'>
+                            <div key={skill._id}>
+                                <h3><Link to={`/skills/${skill._id}`}>{skill.title}</Link></h3>
+                                <img src='https://res.cloudinary.com/da6m0xppc/image/upload/v1615318971/bin_ndnbz3.svg' alt='Delete skill icon' onClick={(skillId)=> this.handleDelete(skill._id)} />
+                                {/* <button onClick={(skillId)=> this.handleDelete(skill._id)}>Delete</button> */}
+                            </div>
+                            <p>{skill.description}</p>
+                        </section>
                     )
                 })}
                 </ul>
-                <Popup trigger={<button> Add new skill </button>} modal>
-                    <AddSkill getAllMySkills={() => this.getMySkills()} />
-                </Popup>
-            </div>
+            </main>
         )
     }
 }
